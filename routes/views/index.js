4ac3fd38a241ecc4f4a7d020a400f7f37787f32c
Isacar Racine <irodrigu@harding.edu>
2018-07-22 17:48:55 -0500

    added integration with Trello API

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,6 +1,6 @@
 var keystone = require('keystone');
 var Request = keystone.list('Request');
-
+var request = require("request");
 exports = module.exports = function (req, res) {
 
 	var view = new keystone.View(req, res);
@@ -13,13 +13,30 @@ exports = module.exports = function (req, res) {
 // *THIS IS NO LONGER USED **/
 	view.on('init', function (next) {
 		//query db with requests from logged in user team
-		console.log(locals);
+		console.log('init TRELLO');
 
-				next();
+		//var options = { method: 'GET', url: 'https://api.trello.com/1/members/iracine/boards/?fields=all,url&key=1d6342088d9a143a39fade05099e8f58&token=afad56f4d0e7e4fe775abeb610359f0e7a7061e465441820dba064b64cd73e5e' };
+		//var options = { method: 'GET', url: 'https://api.trello.com/1/boards/5b4614c7ef558089bd14d2d2/lists/?fields=all,url&key=1d6342088d9a143a39fade05099e8f58&token=afad56f4d0e7e4fe775abeb610359f0e7a7061e465441820dba064b64cd73e5e' };
+		//var options = { method: 'GET', url: 'https://api.trello.com/1/lists/5b4628438c095ee92010c1bd/cards/?fields=all,url&key=1d6342088d9a143a39fade05099e8f58&token=afad56f4d0e7e4fe775abeb610359f0e7a7061e465441820dba064b64cd73e5e' };
+		var options = { method: 'POST',
+		  url: 'https://api.trello.com/1/cards',
+		  qs:
+		   { name: 'Request by request app test',
+		     idList: '5b4628438c095ee92010c1bd',
+			desc: 'please fill this output',
+		     keepFromSource: 'all',
+		     key: '1d6342088d9a143a39fade05099e8f58',
+		     token: 'afad56f4d0e7e4fe775abeb610359f0e7a7061e465441820dba064b64cd73e5e'
+			}
+		};
 
-		
+		request(options, function (error, response, body) {
+		  if (error) throw new Error(error);
+		  console.log(body);
+		})
+		next();
 	});
 
 	// Render the view
-	view.render('teamBoard');
+	view.render('trello');
 };
